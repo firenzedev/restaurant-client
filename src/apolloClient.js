aeb8f88bfab580ced8fd466df1347ea6a49b7908
Lorenzo spinelli <lorenzo.spinelli@registe.it>
2022-05-07 12:38:35 +0200

    fix: api url

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,9 +2,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 
 // Create an http link:
 const httpLink = new HttpLink({
-  // uri: "https://graphql-restaurant-server.herokuapp.com/graphql"
-  // uri: "/graphql"
-  uri: "http://localhost:8080/graphql"
+  uri: "https://graphql-restaurant-server.herokuapp.com/graphql"
+  // uri: "http://localhost:8080/graphql"
 })
 
 const apolloClient = new ApolloClient({
